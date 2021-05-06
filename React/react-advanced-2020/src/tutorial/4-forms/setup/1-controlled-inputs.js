52e2892e76f7b90301bdc28672e2d6c7e88006ff
Sávio de Melo Rocha <44814299+saviorocha@users.noreply.github.com>
2021-05-06 11:29:28 -0300

    Formulários

--- React/react-advanced-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ React/react-advanced-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,63 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      setPeople((people) => {
+        return [...people, person];
+      });
+      setEmail('');
+      setFirstName('');
+    } else {
+      console.log('empty values');
+    }
+  }
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name: </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(evt) => { setFirstName(evt.target.value) }}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(evt) => { setEmail(evt.target.value) }}
+            />
+          </div>
+          <button type="submit">Add Person</button>
+        </form>
+        {
+          people.map((person, index) => {
+            const { id, firstName, email } = person
+            return (
+              <div className="item" key={id}>
+                <h4>{firstName}</h4>
+                <p>{email}</p>
+              </div>
+            );
+          })
+        }
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
